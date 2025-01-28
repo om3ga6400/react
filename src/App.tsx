788781e132b2c33603a09000cb107f87c2767961
Jarza <96431656+JarzaClay@users.noreply.github.com>
2025-01-28 18:48:18 +0000

    Finish background

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BackgroundParticles } from "./components/BackgroundPatricles"
+
+function App() {
+  return (
+    <>
+      <BackgroundParticles />
+      <h1 className="text-amber-400">Welcome to my website</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/BackgroundPatricles.tsx
@@ -0,0 +1,96 @@
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+
+const BackgroundParticles = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (_container?: Container): Promise<void> => {
+    //console.log(_container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#1e1e2e",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 115,
+          enable: true,
+          opacity: 0.25,
+          width: 1,
+        },
+        move: {
+          enable: true,
+          outModes: {
+            default: OutMode.out,
+          },
+          random: false,
+          speed: 1.5,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: 1,
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return <></>;
+};
+
+export { BackgroundParticles }
